366e815e708330bf2b02d325ce8cf76566ef95c0
Alberto Mengali <disberd@gmail.com>
2024-07-01 11:33:31 +0200

    add tests
    Former-commit-id: 1721144654036b9083a299fd5f1d833ef22f8dd9

---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+using TestItemRunner
+
+@testitem "P.839-4 - Rain Height Model" begin
+    include("iturP839test.jl")
+end
+
+@testitem "P.1511-2 - Topography for Earth-space propagation modelling
+" begin
+    include("iturP1511test.jl")
+end
