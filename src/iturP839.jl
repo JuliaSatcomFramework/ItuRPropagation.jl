7303b643eb77cae88aa197395eca1f63b54751b5
Alberto Mengali <disberd@gmail.com>
2024-07-01 11:08:07 +0200

    make globals const
    Former-commit-id: 551d62ddbb40fff749f976bbaf9d357bd7e586e8

--- src/iturP839.jl
+++ src/iturP839.jl
@@ -6,17 +6,17 @@ This Recommendation provides a method to predict the rain height for propagation
 
 using ItuRPropagation
 
-version = ItuRVersion("ITU-R", "P.839", 4, "(09/2013)")
+const version = ItuRVersion("ITU-R", "P.839", 4, "(09/2013)")
 
 #region initialization
 
-latsize = 121 + 1 # number of latitude points (-90, 90, 0.75) plus one extra row for interpolation
-lonsize = 241 + 1 # number of longitude points (-180, 180, 0.75) plus one extra column for interpolation
+const latsize = 121 + 1 # number of latitude points (-90, 90, 0.75) plus one extra row for interpolation
+const lonsize = 241 + 1 # number of longitude points (-180, 180, 0.75) plus one extra column for interpolation
 
-latvalues = [(-90.0 + (i - 1) * 1.5) for i in 1:latsize]
-lonvalues = [(-180.0 + (j - 1) * 1.5) for j in 1:lonsize]
+const latvalues = [(-90.0 + (i - 1) * 1.5) for i in 1:latsize]
+const lonvalues = [(-180.0 + (j - 1) * 1.5) for j in 1:lonsize]
 
-isothermheightdata = zeros(Float64, latsize, lonsize)
+const isothermheightdata = zeros(Float64, latsize, lonsize)
 read!(
     joinpath(@__DIR__, "data/isothermheighth0annual_$(string(latsize))_x_$(string(lonsize)).bin"),
     isothermheightdata
@@ -37,22 +37,10 @@ h0 will be interpolated for the given latitude and longitude.
 - `hR::Real`: mean annual rain height above mean sea level
 """
 function rainheightannual(latlon::LatLon)
-    latrange = searchsorted(latvalues, latlon.lat)
-    lonrange = searchsorted(lonvalues, latlon.lon)
-    R = latrange.stop
-    C = lonrange.stop
-
-    r = ((latlon.lat + 90) / 1.5) + 1
-    c = ((latlon.lon + 180) / 1.5) + 1
-
-    h0 = (
-        isothermheightdata[R, C] * ((R + 1 - r) * (C + 1 - c)) +
-        isothermheightdata[R+1, C] * ((r - R) * (C + 1 - c)) +
-        isothermheightdata[R, C+1] * ((R + 1 - r) * (c - C)) +
-        isothermheightdata[R+1, C+1] * ((r - R) * (c - C))
-    )
+    h0 = isothermheight(latlon)
 
     hR = h0 + 0.36  # equation in section 2
+    return hR
 end
 
 
@@ -83,6 +71,8 @@ function isothermheight(latlon::LatLon)
         isothermheightdata[R, C+1] * ((R + 1 - r) * (c - C)) +
         isothermheightdata[R+1, C+1] * ((r - R) * (c - C))
     )
+
+    return h0
 end
 
 end # module ItuRP839
