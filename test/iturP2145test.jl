a6efeb49cb3da5feb47e0fa0944cce0800906df0
Alberto Mengali <disberd@gmail.com>
2025-06-02 16:17:29 +0200

    add tests per validation excel

--- test/iturP2145test.jl
+++ test/iturP2145test.jl
@@ -14,7 +14,7 @@ end
                 alt = row[3],
                 p = row[4],
                 RHO_mean = row[5],
-                P_mean = row[7],
+                P_mean = row[6],
                 T_mean = row[7],
                 V_mean = row[8],
                 RHO_ccdf = row[9],
@@ -30,7 +30,7 @@ end
     @testset "Average Temperature T̄ₛ" begin
         for entry in entries
             (; ll, alt, T_mean) = entry
-            T̄ₛ = ItuRP2145.surfacetemperatureannual2(ll; alt)
+            T̄ₛ = ItuRP2145.surfacetemperatureannual(ll; alt)
             @test T̄ₛ ≈ T_mean rtol=error_tolerance
         end
     end
@@ -38,16 +38,56 @@ end
     @testset "Average Surface Water Vapour Density ρ̄ₛ" begin
         for entry in entries
             (; ll, alt, RHO_mean) = entry
-            ρ̄ₛ = ItuRP2145.surfacewatervapordensityannual2(ll; alt)
+            ρ̄ₛ = ItuRP2145.surfacewatervapourdensityannual(ll; alt)
             @test ρ̄ₛ ≈ RHO_mean rtol=error_tolerance
+        end
+    end
+
+    @testset "Average Surface Pressure P̄ₛ" begin
+        for entry in entries
+            (; ll, alt, P_mean) = entry
+            P̄ₛ = ItuRP2145.surfacepressureannual(ll; alt)
+            @test P̄ₛ ≈ P_mean rtol=error_tolerance
+        end
+    end
+
+    @testset "Average Surface Integrated Water Vapour Content V̄ₛ" begin
+        for entry in entries
+            (; ll, alt, V_mean) = entry
+            V̄ₛ = ItuRP2145.surfacewatervapourcontentannual(ll; alt)
+            @test V̄ₛ ≈ V_mean rtol=error_tolerance
         end
     end
 
     @testset "CCDF Temperature Tₛ(p)" begin
         for entry in entries
             (; ll, alt, p, T_ccdf) = entry
-            Tₛ = ItuRP2145.surfacetemperatureannual2(ll, p; alt)
+            Tₛ = ItuRP2145.surfacetemperatureannual(ll, p; alt)
             @test Tₛ ≈ T_ccdf rtol=error_tolerance
+        end
+    end
+
+    @testset "CCDF Surface Water Vapour Density ρₛ(p)" begin
+        for entry in entries
+            (; ll, alt, p, RHO_ccdf) = entry
+            ρₛ = ItuRP2145.surfacewatervapourdensityannual(ll, p; alt)
+            @test ρₛ ≈ RHO_ccdf rtol=error_tolerance
+        end
+    end 
+
+    @testset "CCDF Surface Pressure Pₛ(p)" begin
+        for entry in entries
+            (; ll, alt, p, P_ccdf) = entry
+            Pₛ = ItuRP2145.surfacepressureannual(ll, p; alt)
+            @test Pₛ ≈ P_ccdf rtol=error_tolerance
+        end
+    end
+
+    @testset "CCDF Surface Integrated Water Vapour Content Vₛ(p)" begin
+        for entry in entries
+            (; ll, alt, p, V_ccdf) = entry
+            Vₛ = ItuRP2145.surfacewatervapourcontentannual(ll, p; alt)
+            @test Vₛ ≈ V_ccdf rtol=error_tolerance
         end
     end
 end
