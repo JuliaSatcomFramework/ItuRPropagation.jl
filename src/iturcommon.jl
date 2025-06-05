7926cab54519ba9a883ab28d5fe17e6f732a1fcb
Alberto Mengali <disberd@gmail.com>
2025-06-05 09:11:59 +0200

    remove hanging Interpolator structs and add some input processing functions

--- src/iturcommon.jl
+++ src/iturcommon.jl
@@ -22,9 +22,14 @@ struct LatLon
     end
 end
 
-# This functions 
-_torad(val::Real) = deg2rad(val)
-_todeg(val::Real) = val
+# This functions are used for processing inputs and allow easier extension to support types from other packages
+@inline _torad(val::Real) = deg2rad(val)
+@inline _todeg(val::Real) = rad2deg(val)
+
+# We expects length values to be provided in km
+@inline _tokm(val::Real) = val
+
+@inline _tolaton(x) = convert(LatLon, x)::LatLon # Type assertion on convert is used to help compiler, see https://github.com/JuliaLang/julia/issues/42372 for more details
 
 Base.show(io::IO, p::LatLon) = print(io, "(", p.lat, ", ", p.lon, ")")
 
@@ -60,84 +65,4 @@ struct ItuRVersion
     recommendation::String
     dashednumber::Int8
     datestring::String
-end
-
-"""
-    SquareGridInterpolator
-
-Creates an interpolator on a square grid following the bilinear interpolation
-method specified in Section 1b of Annex 1 of ITU-R P.1144-12.
-"""
-struct SquareGridInterpolator{T,R,C}
-    rowrange::R
-    colrange::C
-    data::Matrix{T}
-end
-
-SquareGridInterpolator(latrange, lonrange) = SquareGridInterpolator(latrange, lonrange, fill(NaN, length(latrange), length(lonrange)))
-
-@inline function (itp::SquareGridInterpolator)(R::Integer, C::Integer, r, c)
-    (; data ) = itp
-    δr = r - R
-    δc = c - C
-    latidxs, lonidxs = axes(data)
-    R₊₁ = min(R+1, last(latidxs))
-    C₊₁ = min(C+1, last(lonidxs))
-    DL, UL, DR, UR = data[R, C], data[R₊₁, C], data[R, C₊₁], data[R₊₁, C₊₁]
-    out = (
-        DL * (1 - δr) * (1 - δc) +
-        UL * δr * (1 - δc) + 
-        DR  * (1 - δr) * δc + 
-        UR * δr * δc
-    )
-    return out
-end
-
-function (itp::SquareGridInterpolator)(rowval, colval)
-    R = searchsortedlast(itp.rowrange, rowval)
-    C = searchsortedlast(itp.colrange, colval)
-    Δrow = R === length(itp.rowrange) ? 1 : itp.rowrange[R + 1] - itp.rowrange[R]
-    Δcol = C === length(itp.colrange) ? 1 : itp.colrange[C + 1] - itp.colrange[C]
-    r = R + (rowval - itp.rowrange[R]) / Δrow
-    c = C + (colval - itp.colrange[C]) / Δcol
-    val = itp(R, C, r, c)
-    return val
-end
-
-struct AltitudeScaledSquareGridInterpolator{T,R,C,F}
-    rowrange::R
-    colrange::C
-    data::Matrix{T}
-    scale::Matrix{T}
-    alt::Matrix{T}
-    f::F
-end
-
-@inline function (itp::AltitudeScaledSquareGridInterpolator)(R::Integer, C::Integer, r, c; alt)
-    (; data ) = itp
-    δr = r - R
-    δc = c - C
-    latidxs, lonidxs = axes(data)
-
-    R₊₁ = min(R+1, last(latidxs))
-    C₊₁ = min(C+1, last(lonidxs))
-    iters = ( # idx, Weight
-        (CartesianIndex(R, C), (1 - δr) * (1 - δc)),
-        (CartesianIndex(R₊₁, C), δr * (1 - δc)),
-        (CartesianIndex(R, C₊₁), (1 - δr) * δc),
-        (CartesianIndex(R₊₁, C₊₁), δr * δc)
-    )
-    # Function to compute Xᵢ
-    fun(i) = let itp=itp, alt = alt
-        Xᵢ′ = itp.data[i]
-        scaleᵢ = itp.scale[i]
-        altᵢ = itp.alt[i]
-        itp.f(Xᵢ′, alt, scaleᵢ, altᵢ)
-    end
-    out = 0.0
-    @inbounds @simd for el in iters
-        idx, weight = el
-        out += fun(idx) * weight
-    end
-    return out
 end
